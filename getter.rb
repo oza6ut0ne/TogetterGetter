d2abaf0b293223d6ca0c163e958caaefcaa6c7cd
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2019-04-17 20:25:02 +0900

    make sure to rescue LoadError too

--- getter.rb
+++ getter.rb
@@ -17,7 +17,7 @@ begin
     end
     sess = Capybara::Session.new :selenium
     sess.visit url
-rescue
+rescue Exception
     require 'capybara/poltergeist'
     Capybara.register_driver :poltergeist do |app|
         Capybara::Poltergeist::Driver.new(app, js_errors: false)
