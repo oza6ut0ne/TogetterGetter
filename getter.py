9c119740d748bde2ae9bbe538a98fe8b17c38736
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2023-11-05 21:59:37 +0900

    Update getter.py for Selenium 4.14.0

--- getter.py
+++ getter.py
@@ -1,10 +1,13 @@
 import argparse
-import pyquery
 import re
 import selenium.webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import WebDriverException
+
+
+def click(driver, element):
+    driver.execute_script('arguments[0].click();', element)
 
 
 parser = argparse.ArgumentParser()
@@ -12,34 +15,30 @@ parser.add_argument('url')
 parser.add_argument('-o', '--one-by-one', action='store_true')
 args = parser.parse_args()
 
-if re.match('\d+', args.url):
+if re.match(r'\d+', args.url):
     args.url = r'https://togetter.com/li/' + args.url
 
-try:
-    options = Options()
-    options.headless = True
-    options.add_argument('--log-level=2')
-    driver = selenium.webdriver.Chrome(chrome_options=options)
-except WebDriverException:
-    driver = selenium.webdriver.PhantomJS()
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--log-level=2')
+driver = selenium.webdriver.Chrome(options=options)
 
 driver.get(args.url)
 
 while True:
     while True:
         try:
-            more_tweet_btn = driver.find_element_by_id('more_tweet_btn')
-            more_tweet_btn.click()
+            more_tweet_btn = driver.find_element(By.ID, 'more_tweet_btn')
+            click(driver, more_tweet_btn)
         except NoSuchElementException:
             break
 
-    pq = pyquery.PyQuery(driver.page_source)
-    for tweet in pq.find('.tweet_box .tweet'):
+    for tweet in driver.find_elements(By.CSS_SELECTOR, '.tweet_box .tweet'):
         print(tweet.text)
         input() if args.one_by_one else print()
 
     try:
-        next_btn = driver.find_element_by_css_selector('div.pagenation a[rel~=next]')
-        next_btn.click()
+        next_btn = driver.find_element(By.CSS_SELECTOR, 'div.pagenation a[rel~=next]')
+        click(driver, next_btn)
     except NoSuchElementException:
         break
